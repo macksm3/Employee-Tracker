5ab9d64f157267c720c12712adaf56dfa58fd908
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-09-13 19:43:42 -0400

    add sql files and readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Employee-Tracker
 homework 12
+## Description
+  Using CLI to manage an employee database based on inquirer prompt input.
+  User can view departments, roles and employees, add new departments, roles and employees and update employee roles. 
+  Modules used include inquirer, mysql and console.table.
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributers](#contributers)
+  * [License](#license)
+  
+  ## Installation
+  Run npm i to install node modules listed in package.json file
+  
+  
+  ## Usage
+  CLI asks what task you would like to perform, then guides you through the steps to complete the selected task. 
+  
+  
+  ## Contributers
+  Macks 
+  
+  
+  ## License
+  This project is licensed under the terms of the PDDL license. 
+  
+  
+  ## Badges
+  
+  [![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)
+  
+ 
--- empTrackerSeeds.sql
+++ empTrackerSeeds.sql
@@ -1,29 +1,13 @@
-DROP DATABASE IF EXISTS empTracker_DB;
-CREATE DATABASE empTracker_DB;
 
-USE empTracker_DB;
+INSERT INTO departments (dept_name) VALUES ('Sales');
+INSERT INTO departments (dept_name) VALUES ('Finance');
+INSERT INTO departments (dept_name) VALUES ('Legal');
 
-CREATE TABLE departments(
-  id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
-);
+INSERT INTO role (title, salery, department_id) VALUES ('Sales Lead', 100000, 1);
+INSERT INTO role (title, salery, department_id) VALUES ('Account', 125000, 2);
+INSERT INTO role (title, salery, department_id) VALUES ('Lawyer', 190000, 3);
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salery DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('George', 'Washington', 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Alexander', 'Hamilton', 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('James', 'Madison', 3);
 
